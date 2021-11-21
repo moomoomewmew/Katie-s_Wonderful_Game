3e32c3ce6ff8d9ce8945f97531ebb6752726a81d
Katie Vermazani <katievermazani@tuta.io>
2021-11-21 14:09:33 -0700

    for real

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 /////
 /*==============variables================*/
 const allSquares = document.querySelectorAll('.light-square')
-let red = document.querySelectorAll('.red')
-let black = document.querySelectorAll('.black')
 let activePlayer
 let player = 'red-player'
-
 /*==============This sets up the board===============*/
 for (let i = 0; i < 12; i++) {
     allSquares[i].classList.add('black')
@@ -13,55 +10,64 @@ for (let i = 0; i < 12; i++) {
 for (let i = 20; i < allSquares.length; i++) {
     allSquares[i].classList.add('red')
 }
-const allTokens = document.querySelectorAll('.black, .red')
-for (let i = 0; i < allTokens.length; i++) {
-    allTokens[i].addEventListener('click', play)
+
+let reds = document.querySelectorAll('.red')
+let blacks = document.querySelectorAll('.black')
+
+
+/*==============to switch turns================*/
+// const changeClass = (e) => {
+//     console.log(e.currentTarget.id)
+//     e.currentTarget.classList.add(`${player}`)
+//   }
+
+// for (let i = 0; i < allSquares.length; i++) {
+//     allSquares[i].addEventListener('click', changeClass)
+// }
+
+/*===============to make a move=================*/
+
+if (player === 'red-player') {
+    const reSelectRed = (e) => {
+        const activeRed= document.querySelector('.active-red')
+        activeRed.classList.remove('active-red')
+    }
+    for (let i = 0; i < reds.length; i++) {
+        reds[i].addEventListener('click', reSelectRed)
+    }
+    const selectRed = (e) => {
+        e.currentTarget.classList.add('active-red') 
+        player = 'active-red-player'
+    }
+    for (let i = 0; i < reds.length; i++) {
+        reds[i].addEventListener('click', selectRed)
+    } 
+
+} else if (player === 'black-player'){
+    const reSelectBlack = (e) => {
+        const activeBlack= document.querySelector('.active-black')
+        activeBlack.classList.remove('active-black')
+    }
+    for (let i = 0; i < blacks.length; i++) {
+        blacks[i].addEventListener('click', reSelectBlack)
+    }
+    
+    const selectBlack = (e) => {
+        e.currentTarget.classList.add(`active-black`) 
+    }
+    for (let i = 0; i < blacks.length; i++) {
+        blacks[i].addEventListener('click', selectBlack)
+    }
 }
 
-
-switch play(e) {
-    case (player === 'red-player' && e.target.classList.contains('red')) {
-        e.target.classList.add('active-red') 
-        player = 'active-red-player'       
-        break
-    case (player === 'active-red-player')
-        const reSelectRed = (e) => {
-            const activeRed= document.querySelector('.active-red')
-            if (activeRed){
-            activeRed.classList.remove('active-red')
-            player = 'black-player'
-            }
-        }
-        for (let i = 0; i < red.length; i++) {
-            red[i].addEventListener('click', reSelectRed)
-        }
-        break
-    case (player === 'black-player'){
-        const reSelectBlack = (e) => {
-            const activeBlack= document.querySelector('.active-black')
-            activeBlack.classList.remove('active-black')
-            player === 'active-black-player'
-        }
-        for (let i = 0; i < black.length; i++) {
-            black[i].addEventListener('click', reSelectBlack)
-        }
-        break
-    case (player === 'active-black-player')    
-        const selectBlack = (e) => {
-            e.target.classList.add(`active-black`) 
-        }    
-        for (let i = 0; i < black.length; i++) {
-            black[i].addEventListener('click', selectBlack)
-        }
-        break
-    }
-
-//     if (player === 'active-red-player'){
-
-//         const activeRed = document.querySelector('.active-red')
-//         activeRed.classList.remove('red')
-//         activeRed.classList.remove('active-red')
-//         e.target.classList.add('red')
+// if (player === 'active-red-player'){
+//     const moveRed = (e) => {
+//         const activeRed= document.querySelector('.active-red')
+//         activeRed.classList.remove('active-red', 'red')
+//         e.currentTarget.classList.add('red')
+//     }
+//     for (let i = 0; i < reds.length; i++) {
+//         reds[i].addEventListener('click', moveRed)
 //     }
 // }
 
@@ -72,10 +78,10 @@ switch play(e) {
 
 
 
-// // if (player === '.active-red-player') {
-// //     const moveRedPiece = (e) => {
-// //         // e.currentTarget.classList.add('red')
-// //         // console.log(e.currentTarget)
-// //         // 
-// //         // console.log(e.currentTarget)
+// if (player === '.active-red-player') {
+//     const moveRedPiece = (e) => {
+//         // e.currentTarget.classList.add('reds')
+//         // console.log(e.currentTarget)
+//         // 
+//         // console.log(e.currentTarget)
 
